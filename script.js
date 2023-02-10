fa859cad70c24fe423544c23730d877276416217
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-02-10 18:55:03 +0600

    upload

---
+++ script.js
@@ -0,0 +1,58 @@
+(function ($){
+
+    // for active and remove 
+       $(".mainmenu li").on("click", function (){
+        $(".mainmenu li").removeClass("active");
+        $(this).addClass("active");
+    })
+
+
+    // jason api get methode 
+    $(window).on("load", function() {
+        $.get (
+            "https://perfectedgetech.com/all-dev-projects/perfectjob-admin/api/country-list",
+            function(data, status) {
+                console.log(data);
+                if (status == "success") {
+                    const getCountry = data.data.country_list;
+
+
+                    // (for loop way:
+                    // const country_keys = Object.keys(getCountry);
+                    // for(i = 0; i < country_keys.length; i++) {
+                    //     console.log(country_keys[i])
+                    // }
+
+
+                    //for in loop way : =>
+                    for (let x in getCountry) {
+                        // console.log(getCountry[x])
+                        $(".main-list").append(`<li> ${getCountry[x]} </li>`)
+                        
+                    }
+                }
+
+            }
+        )
+    })
+
+
+
+// $("#mouse-event").on('click',(event)=>{
+//     alert("clicked");
+// })
+
+
+function clickfunction () {
+    document.getElementById('mouse-event').innerHTML = alert("hello")
+}
+
+
+
+
+
+})(window.jQuery);
+
+
+
+
